91c3d52214fa792d11ab07f7657067af7f098311
YoanAbdoulESEO <yoan.abdoul@reseau.eseo.fr>
2025-12-15 15:00:49 +0100

    add periodic fetching of parking data in ParkingVoitureCubit and cancel timer on close

--- angers_connect/lib/blocs/parking_voiture_cubit.dart
+++ angers_connect/lib/blocs/parking_voiture_cubit.dart
@@ -1,12 +1,15 @@
+import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../models/parking_voiture.dart';
 import '../repositories/parking_voiture_repository.dart';
 
 class ParkingVoitureCubit extends Cubit<List<ParkingVoiture>> {
   final ParkingVoitureRepository _repository;
+  Timer? _timer;
 
   ParkingVoitureCubit(this._repository) : super([]) {
     fetchParkings();
+    _timer = Timer.periodic(const Duration(minutes: 1), (_) => fetchParkings());
   }
 
   Future<void> fetchParkings() async {
@@ -16,5 +19,11 @@ class ParkingVoitureCubit extends Cubit<List<ParkingVoiture>> {
     } catch (e) {
       emit([]);
     }
+  }
+
+  @override
+  Future<void> close() {
+    _timer?.cancel();
+    return super.close();
   }
 }
